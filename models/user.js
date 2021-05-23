3912b516d88119431d031085862b155896bdacef
Alexander Holub <holub.alexander.it@gmail.com>
2021-05-23 22:42:02 +0300

    fix: fix compilation, logic errors, refactor user, usercontroller

--- models/user.js
+++ models/user.js
@@ -1,26 +1,32 @@
-module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('user', {
-        full_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+const { Sequelize, DataTypes } = require('sequelize');
 
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+const sequelize = new Sequelize('sqlite::memory:');
 
-        passwordHash: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+// class User extends Model {}
 
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isEmail: true
-            }
-        }
-    })
-}
+const User = sequelize.define('user', {
+  full_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  passwordHash: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
+
+exports.module = User;
